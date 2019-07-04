5bc325cafa35bbe635b3870caedbe895157888e1
Jason R. Banfelder <jason@banfelder.net>
2019-07-04 19:48:30 -0400

    Add get_t_dew_point_from_vap_pres and get_vap_pres_from_t_dew_point functions

--- src/r/tests/testthat/test-dew_point_or_relative_humidity_and_vapor_pressure_conversions.R
+++ src/r/tests/testthat/test-dew_point_or_relative_humidity_and_vapor_pressure_conversions.R
@@ -15,3 +15,65 @@ test_that("expected relationships between vapor pressure and relative humidity h
   rel_hum <- get_rel_hum_from_vap_pres(25.0, vap_pres)
   expect_equal_rel(rel_hum, 0.8, tolerance = 0.0003)
 })
+
+# Test of relationships between vapour pressure and dew point temperature in IP units
+# No need to test vapour pressure calculation as it is just the saturation vapour pressure tested elsewhere
+test_that("reciprocal relationships between vapor pressure and sew point hold in IP units", {
+  set_unit_system("IP")
+  vap_pres <- get_vap_pres_from_t_dew_point(-4.0)
+  expect_equal_abs(get_t_dew_point_from_vap_pres(59.0, vap_pres), -4.0, tolerance = 0.001)
+  vap_pres <- get_vap_pres_from_t_dew_point(41.0)
+  expect_equal_abs(get_t_dew_point_from_vap_pres(59.0, vap_pres), 41.0, tolerance = 0.001)
+  vap_pres <- get_vap_pres_from_t_dew_point(122.0)
+  expect_equal_abs(get_t_dew_point_from_vap_pres(140.0, vap_pres), 122.0, tolerance = 0.001)
+})
+
+# Test of relationships between vapour pressure and dew point temperature in SI units
+# No need to test vapour pressure calculation as it is just the saturation vapour pressure tested above
+test_that("reciprocal relationships between vapor pressure and sew point hold in IP units", {
+  set_unit_system("SI")
+  vap_pres <- get_vap_pres_from_t_dew_point(-20.0)
+  expect_equal_abs(get_t_dew_point_from_vap_pres(15.0, vap_pres), -20.0, tolerance = 0.001)
+  vap_pres <- get_vap_pres_from_t_dew_point(5.0)
+  expect_equal_abs(get_t_dew_point_from_vap_pres(15.0, vap_pres), 5.0, tolerance = 0.001)
+  vap_pres <- get_vap_pres_from_t_dew_point(50.0)
+  expect_equal_abs(get_t_dew_point_from_vap_pres(60.0, vap_pres), 50.0, tolerance = 0.001)
+})
+
+# Test that the NR in GetTDewPointFromVapPres converges in IP units.
+# This test was known problem in versions of PsychroLib <= 2.0.0
+# test_that("the NR in GetTDewPointFromVapPres converges in IP units", {
+#
+#   set_unit_system("IP")
+#
+#   t_dry_bulb <- seq(-148.0, 392.0, by = 1.0)
+#   rel_hum <- seq(0, 1, by = 0.1)
+#   pressure <- seq(8.6, 17.4, by = 1.0)
+#
+#   for (t in t_dry_bulb) {
+#     for (rh in rel_hum) {
+#       for (p in pressure) {
+#         get_t_wet_bulb_from_rel_hum(t, rh, p)   # TODO: why call this, and how to assert that this converges?
+#       }
+#     }
+#   }
+# })
+
+# Test that the NR in GetTDewPointFromVapPres converges in SI units.
+# This test was known problem in versions of PsychroLib <= 2.0.0
+# test_that("the NR in GetTDewPointFromVapPres converges in SI units", {
+#
+#   set_unit_system("SI")
+#
+#   t_dry_bulb <- seq(-100.0, 200.0, by = 1.0)
+#   rel_hum <- seq(0, 1, by = 0.1)
+#   pressure <- seq(60000.0, 120000.0, by = 1.0)
+#
+#   for (t in t_dry_bulb) {
+#     for (rh in rel_hum) {
+#       for (p in pressure) {
+#         get_t_wet_bulb_from_rel_hum(t, rh, p)   # TODO: why call this, and how to assert that this converges?
+#       }
+#     }
+#   }
+# })
