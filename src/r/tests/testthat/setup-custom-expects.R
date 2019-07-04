29430fdd026a294ab9395d66faa1c19277653b46
Jason R. Banfelder <jason@banfelder.net>
2019-07-03 23:34:56 -0400

    Fix expect_equal_rel to deal properly with negative expected values.

--- src/r/tests/testthat/setup-custom-expects.R
+++ src/r/tests/testthat/setup-custom-expects.R
@@ -1,7 +1,7 @@
 # Behave like pytest.approx when used with rel option
 # TODO: confirm this works as we think it does.
 expect_equal_rel <- function(object, expected, tolerance) {
-  expect_equal(object, expected, tolerance, scale = expected)
+  expect_equal(object, expected, tolerance, scale = abs(expected))
 }
 
 # Behave like pytest.approx when used with abs option
