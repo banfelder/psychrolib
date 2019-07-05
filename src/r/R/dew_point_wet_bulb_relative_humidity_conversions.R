41885b8dee3060c0fa31bbea760e01f789e43453
Jason R. Banfelder <jason@banfelder.net>
2019-07-05 15:47:17 -0400

    Add get_t_dew_point_from_t_wet_bulb function

--- src/r/R/dew_point_wet_bulb_relative_humidity_conversions.R
+++ src/r/R/dew_point_wet_bulb_relative_humidity_conversions.R
@@ -107,3 +107,33 @@ get_t_dew_point_from_rel_hum <- function(t_dry_bulb, rel_hum) {
   vap_pres <- get_vap_pres_from_rel_hum(t_dry_bulb, rel_hum)
   get_t_dew_point_from_vap_pres(t_dry_bulb, vap_pres)
 }
+
+#' Return dew-point temperature given dry-bulb temperature, wet-bulb temperature, and pressure.
+#'
+#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param t_wet_bulb numeric Wet-bulb temperature in °F [IP] or °C [SI]
+#' @param pressure numeric Atmospheric pressure in Psi [IP] or Pa [SI]
+#'
+#' @return numeric Dew-point temperature in °F [IP] or °C [SI]
+#'
+#' Reference:
+#'   ASHRAE Handbook - Fundamentals (2017) ch. 1
+#' @export
+get_t_dew_point_from_t_wet_bulb <- function(t_dry_bulb, t_wet_bulb, pressure) {
+
+  if(t_wet_bulb > t_dry_bulb) {
+    stop("Wet bulb temperature is above dry bulb temperature")
+  }
+
+  hum_ratio <- get_hum_ratio_from_t_wet_bulb(t_dry_bulb, t_wet_bulb, pressure)
+  get_t_dew_point_from_hum_ratio(t_dry_bulb, hum_ratio, pressure)
+}
+
+# def GetTDewPointFromTWetBulb(TDryBulb: float, TWetBulb: float, Pressure: float) -> float:
+#
+# if TWetBulb > TDryBulb:
+#   raise ValueError("Wet bulb temperature is above dry bulb temperature")
+#
+# HumRatio = GetHumRatioFromTWetBulb(TDryBulb, TWetBulb, Pressure)
+# TDewPoint = GetTDewPointFromHumRatio(TDryBulb, HumRatio, Pressure)
+# return TDewPoint
