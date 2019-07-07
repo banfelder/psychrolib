c3d5a2340135e54b7d4c61bdfe378790bd84284f
Jason R. Banfelder <jason@banfelder.net>
2019-07-06 23:46:20 -0400

    Tidy roxygen in dew_point_wet_bulb_relative_humidity_conversions.R

--- src/r/R/dew_point_wet_bulb_relative_humidity_conversions.R
+++ src/r/R/dew_point_wet_bulb_relative_humidity_conversions.R
@@ -4,13 +4,13 @@
 
 #' Return wet-bulb temperature given dry-bulb temperature, dew-point temperature, and pressure.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param t_dew_point numeric Dew-point temperature in °F [IP] or °C [SI]
-#' @param pressure numeric Atmospheric pressure in Psi [IP] or Pa [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param t_dew_point Dew-point temperature in °F [IP] or °C [SI]
+#' @param pressure Atmospheric pressure in Psi [IP] or Pa [SI]
 #'
-#' @return numeric Wet-bulb temperature in °F [IP] or °C [SI]
+#' @return Wet-bulb temperature in °F [IP] or °C [SI]
 #'
-#' Reference:
+#' @section Reference:
 #'   ASHRAE Handbook - Fundamentals (2017) ch. 1
 #' @export
 get_t_wet_bulb_from_t_dew_point <- function(t_dry_bulb, t_dew_point, pressure) {
@@ -25,13 +25,13 @@ get_t_wet_bulb_from_t_dew_point <- function(t_dry_bulb, t_dew_point, pressure) {
 
 #' Return wet-bulb temperature given dry-bulb temperature, relative humidity, and pressure.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param rel_hum numeric Relative humidity in range [0, 1]
-#' @param pressure numeric Atmospheric pressure in Psi [IP] or Pa [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param rel_hum Relative humidity in range [0, 1]
+#' @param pressure Atmospheric pressure in Psi [IP] or Pa [SI]
 #'
-#' @return numeric Wet-bulb temperature in °F [IP] or °C [SI]
+#' @return Wet-bulb temperature in °F [IP] or °C [SI]
 #'
-#' Reference:
+#' @section Reference:
 #'   ASHRAE Handbook - Fundamentals (2017) ch. 1
 #' @export
 get_t_wet_bulb_from_rel_hum <- function(t_dry_bulb, rel_hum, pressure) {
@@ -47,12 +47,12 @@ get_t_wet_bulb_from_rel_hum <- function(t_dry_bulb, rel_hum, pressure) {
 
 #' Return relative humidity given dry-bulb temperature and dew-point temperature.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param t_dew_point numeric Dew-point temperature in °F [IP] or °C [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param t_dew_point Dew-point temperature in °F [IP] or °C [SI]
 #'
-#' @return numeric Relative humidity in range [0, 1]
+#' @return Relative humidity in range [0, 1]
 #'
-#' Reference:
+#' @section Reference:
 #'   ASHRAE Handbook - Fundamentals (2017) ch. 1 eqn 22
 #' @export
 get_rel_hum_from_t_dew_point <- function(t_dry_bulb, t_dew_point) {
@@ -69,13 +69,13 @@ get_rel_hum_from_t_dew_point <- function(t_dry_bulb, t_dew_point) {
 
 #' Return relative humidity given dry-bulb temperature, wet bulb temperature and pressure.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param t_wet_bulb numeric Wet-bulb temperature in °F [IP] or °C [SI]
-#' @param pressure numeric Atmospheric pressure in Psi [IP] or Pa [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param t_wet_bulb Wet-bulb temperature in °F [IP] or °C [SI]
+#' @param pressure Atmospheric pressure in Psi [IP] or Pa [SI]
 #'
-#' @return numeric Relative humidity in range [0, 1]
+#' @return Relative humidity in range [0, 1]
 #'
-#' Reference:
+#' @section Reference:
 #'   ASHRAE Handbook - Fundamentals (2017) ch. 1
 #' @export
 get_rel_hum_from_t_wet_bulb <- function(t_dry_bulb, t_wet_bulb, pressure) {
@@ -90,12 +90,12 @@ get_rel_hum_from_t_wet_bulb <- function(t_dry_bulb, t_wet_bulb, pressure) {
 
 #' Return dew-point temperature given dry-bulb temperature and relative humidity.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param rel_hum numeric Relative humidity in range [0, 1]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param rel_hum Relative humidity in range [0, 1]
 #'
-#' @return numeric Dew-point temperature in °F [IP] or °C [SI]
+#' @return Dew-point temperature in °F [IP] or °C [SI]
 #'
-#' Reference:
+#' @section Reference:
 #'   ASHRAE Handbook - Fundamentals (2017) ch. 1
 #' @export
 get_t_dew_point_from_rel_hum <- function(t_dry_bulb, rel_hum) {
@@ -110,13 +110,13 @@ get_t_dew_point_from_rel_hum <- function(t_dry_bulb, rel_hum) {
 
 #' Return dew-point temperature given dry-bulb temperature, wet-bulb temperature, and pressure.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param t_wet_bulb numeric Wet-bulb temperature in °F [IP] or °C [SI]
-#' @param pressure numeric Atmospheric pressure in Psi [IP] or Pa [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param t_wet_bulb Wet-bulb temperature in °F [IP] or °C [SI]
+#' @param pressure Atmospheric pressure in Psi [IP] or Pa [SI]
 #'
-#' @return numeric Dew-point temperature in °F [IP] or °C [SI]
+#' @return Dew-point temperature in °F [IP] or °C [SI]
 #'
-#' Reference:
+#' @section Reference:
 #'   ASHRAE Handbook - Fundamentals (2017) ch. 1
 #' @export
 get_t_dew_point_from_t_wet_bulb <- function(t_dry_bulb, t_wet_bulb, pressure) {
