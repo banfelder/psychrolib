1e98d23c6b98db569f61fb55147e33796b30920f
Jason R. Banfelder <jason@banfelder.net>
2019-07-07 11:55:54 -0400

    Tidy roxygen in moist_air_calculations.R

--- src/r/R/moist_air_calculations.R
+++ src/r/R/moist_air_calculations.R
@@ -4,13 +4,13 @@
 
 #' Return Vapor pressure deficit given dry-bulb temperature, humidity ratio, and pressure.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param hum_ratio numeric Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
-#' @param pressure numeric Atmospheric pressure in Psi [IP] or Pa [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param hum_ratio Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
+#' @param pressure Atmospheric pressure in Psi [IP] or Pa [SI]
 #'
-#' @return numeric Vapor pressure deficit in Psi [IP] or Pa [SI]
+#' @return Vapor pressure deficit in Psi [IP] or Pa [SI]
 #'
-#' Reference:
+#' @section Reference:
 #'   Oke (1987) eqn 2.13a
 #' @export
 get_vapor_pressure_deficit <- function(t_dry_bulb, hum_ratio, pressure) {
@@ -24,17 +24,16 @@ get_vapor_pressure_deficit <- function(t_dry_bulb, hum_ratio, pressure) {
 #' Return the degree of saturation (i.e humidity ratio of the air / humidity ratio of the air at saturation
 #' at the same temperature and pressure) given dry-bulb temperature, humidity ratio, and atmospheric pressure.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param hum_ratio numeric Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
-#' @param pressure numeric Atmospheric pressure in Psi [IP] or Pa [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param hum_ratio Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
+#' @param pressure Atmospheric pressure in Psi [IP] or Pa [SI]
 #'
-#' @return numeric Degree of saturation in arbitrary unit
+#' @return Degree of saturation in arbitrary unit
 #'
-#' Reference:
+#' @section Reference:
 #'   ASHRAE Handbook - Fundamentals (2009) ch. 1 eqn 12
+#'   (this definition is absent from the 2017 Handbook)
 #'
-#' Notes:
-#'   This definition is absent from the 2017 Handbook. Using 2009 version instead.
 #' @export
 get_degree_of_saturation <- function(t_dry_bulb, hum_ratio, pressure) {
 
@@ -50,12 +49,12 @@ get_degree_of_saturation <- function(t_dry_bulb, hum_ratio, pressure) {
 
 #' Return moist air enthalpy given dry-bulb temperature and humidity ratio.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param hum_ratio numeric Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param hum_ratio Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
 #'
-#' @return numeric Moist air enthalpy in Btu lb⁻¹ [IP] or J kg⁻¹
+#' @return Moist air enthalpy in Btu lb⁻¹ [IP] or J kg⁻¹
 #'
-#' Reference:
+#' @section Reference:
 #'  ASHRAE Handbook - Fundamentals (2017) ch. 1 eqn 30
 #' @export
 get_moist_air_enthalpy <- function(t_dry_bulb, hum_ratio) {
@@ -76,18 +75,15 @@ get_moist_air_enthalpy <- function(t_dry_bulb, hum_ratio) {
 
 #' Return moist air specific volume given dry-bulb temperature, humidity ratio, and pressure.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param hum_ratio numeric Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param hum_ratio Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
 #' @param pressure Atmospheric pressure in Psi [IP] or Pa [SI]
 #'
-#' @return numeric Specific volume of moist air in ft³ lb⁻¹ of dry air [IP] or in m³ kg⁻¹ of dry air [SI]
+#' @return Specific volume of moist air in ft³ lb⁻¹ of dry air [IP] or in m³ kg⁻¹ of dry air [SI]
 #'
-#' Reference:
+#' @section Reference:
 #'   ASHRAE Handbook - Fundamentals (2017) ch. 1 eqn 26
 #'
-#' Notes:
-#'   In IP units, R_DA_IP / 144 equals 0.370486 which is the coefficient appearing in eqn 26
-#'   The factor 144 is for the conversion of Psi = lb in⁻² to lb ft⁻².
 #' @export
 get_moist_air_volume <- function(t_dry_bulb, hum_ratio, pressure) {
 
@@ -97,6 +93,8 @@ get_moist_air_volume <- function(t_dry_bulb, hum_ratio, pressure) {
   bounded_hum_ratio <- max(hum_ratio, MIN_HUM_RATIO)
 
   if(is_ip()) {
+    # R_DA_IP / 144 equals 0.370486 which is the coefficient appearing in eqn 26
+    # The factor 144 is for the conversion of Psi = lb in⁻² to lb ft⁻².
     moist_air_volume <- R_DA_IP * get_t_rankine_from_t_fahrenheit(t_dry_bulb) * (1 + 1.607858 * bounded_hum_ratio) / (144 * pressure)
   } else {
     moist_air_volume <- R_DA_SI * get_t_kelvin_from_t_celsius(t_dry_bulb) * (1 + 1.607858 * bounded_hum_ratio) / pressure
@@ -106,13 +104,13 @@ get_moist_air_volume <- function(t_dry_bulb, hum_ratio, pressure) {
 
 #' Return moist air density given humidity ratio, dry bulb temperature, and pressure.
 #'
-#' @param t_dry_bulb numeric Dry-bulb temperature in °F [IP] or °C [SI]
-#' @param hum_ratio numeric Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
-#' @param pressure numeric Atmospheric pressure in Psi [IP] or Pa [SI]
+#' @param t_dry_bulb Dry-bulb temperature in °F [IP] or °C [SI]
+#' @param hum_ratio Humidity ratio in lb_H₂O lb_Air⁻¹ [IP] or kg_H₂O kg_Air⁻¹ [SI]
+#' @param pressure Atmospheric pressure in Psi [IP] or Pa [SI]
 #'
-#' @return numeric MoistAirDensity: Moist air density in lb ft⁻³ [IP] or kg m⁻³ [SI]
+#' @return MoistAirDensity: Moist air density in lb ft⁻³ [IP] or kg m⁻³ [SI]
 #'
-#' Reference:
+#' @section Reference:
 #'   ASHRAE Handbook - Fundamentals (2017) ch. 1 eqn 11
 #' @export
 get_moist_air_density <- function(t_dry_bulb, hum_ratio, pressure) {
